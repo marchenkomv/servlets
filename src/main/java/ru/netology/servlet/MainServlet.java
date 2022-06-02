b2d249b8bb497180d2743065607b273502945995
marchenkomv <marchenko_m.v@mail.ru>
2022-06-02 13:15:41 +1000

    add java-annotation

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.exception.NotFoundException;
 import ru.netology.repository.PostRepository;
@@ -16,9 +18,10 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        final var repository = context.getBean(PostRepository.class);
+        final var service = context.getBean(PostService.class);
+        controller = context.getBean(PostController.class);
     }
 
     @Override
